5abf6237fc6d90c2c371f52549d691fff6ac1fd8
tristan.gremont <tristan.gremont@epitech.eu>
2021-02-12 21:23:06 +0100

    [add] manage gui

--- src/utils/scene/components_parsing.c
+++ src/utils/scene/components_parsing.c
@@ -9,7 +9,7 @@
 
 static void insert_pt2(entity_t *entity, scene_objects_t *objects)
 {
-    if (E_TEXT)
+    if (E_TEXT && !E_RSPRITE)
         add_entity_to_list(entity, &objects->text);
     if (E_SCRIPT)
         add_entity_to_list(entity, &objects->scripts);
@@ -17,25 +17,29 @@ static void insert_pt2(entity_t *entity, scene_objects_t *objects)
         add_entity_to_list(entity, &objects->interact);
 }
 
-void insert_entity_inlists(entity_t *entity, scene_objects_t *objects)
+void insert_entity_inlists(entity_t *entity, scene_objects_t *objects
+, sfBool is_gui)
 {
     if (E_COLLIDER)
         add_entity_to_list(entity, &objects->colliders);
-    if (E_RSPRITE)
+    if (E_RSPRITE && !is_gui)
         add_entity_to_list(entity, &objects->render);
     if (E_SOUND)
         add_entity_to_list(entity, &objects->sfx);
     insert_pt2(entity, objects);
 }
 
-void insert_entities_inlists(entity_t *list, scene_objects_t *objects)
+void insert_entities_inlists(entity_t *list, scene_objects_t *objects,
+sfBool is_gui)
 {
     entity_t *entity = list;
 
     for (; entity; entity = entity->next) {
         if (list->children)
-            insert_entities_inlists(entity->children, objects);
-        insert_entity_inlists(entity, objects);
+            insert_entities_inlists(entity->children, objects, is_gui);
+        insert_entity_inlists(entity, objects, is_gui);
+        if (E_TRANSFORM)
+            add_entity_to_list(entity, &objects->transforms);
     }
 }
 
@@ -43,6 +47,6 @@ void init_scenes(scenes_t *scenes)
 {
     for (int i = 0; i < scenes->count; i++) {
         insert_entities_inlists(scenes->list[i].objects->list,
-        scenes->list[i].objects);
+        scenes->list[i].objects, sfFalse);
     }
 }

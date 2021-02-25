ca52f14a4834bb32ec06c88e6b29abbbfd11cc6d
tristan.gremont <tristan.gremont@epitech.eu>
2021-02-25 17:47:38 +0100

    [fix] engine issues

--- src/scripts_engine/scripts_updates.c
+++ src/scripts_engine/scripts_updates.c
@@ -19,7 +19,7 @@ void update_script(game_data_t *data, entity_list_t *entity, script_t *script)
         return;
     if (script->time_dependent == ON &&
     my_timer(G_CLOCK, script->trigger, &script->actual )) {
-        script->update(script->data, data, entity);
+        script->update(&(script->data), data, entity);
     } else if (script->time_dependent == OFF) {
         script->update(&script->data, data, entity);
     }
